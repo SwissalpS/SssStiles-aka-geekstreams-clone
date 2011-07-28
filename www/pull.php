089d81ab449d9037e5bb32ecf8ff648b5cd71866
SwissalpS <Luke@SwissalpS.ws>
2011-07-28 23:43:44 +0200

    the essential files for public_html

---
+++ www/pull.php
@@ -0,0 +1,17 @@
+<?php
+/* * * *
+ * * pull.php
+ * * respond to ajax requests with xml output
+ * *
+ * * @version 20110725_035145 (CC) Luke JZ aka SwissalpS
+ * * * */
+//error_reporting(E_ALL); // 0); //
+
+require_once('/home/tiles/conf/SssSphocoaApp.conf');
+
+SssStilesServer::pull();
+
+exit(0);
+
+/* * * *\ pull.php (CC) Luke JZ aka SwissalpS /* * * */
+?>
---
+++ www/push.php
@@ -0,0 +1,17 @@
+<?php
+/* * * *
+ * * push.php
+ * * apply settings to the tiles
+ * *
+ * * @version 20110725_130725 (CC) Luke JZ aka SwissalpS
+ * * * */
+//error_reporting(E_ALL); // 0); //
+
+require_once('/home/tiles/conf/SssSphocoaApp.conf');
+
+SssStilesServer::push();
+
+exit(0);
+
+/* * * *\ push.php (CC) Luke JZ aka SwissalpS /* * * */
+?>
